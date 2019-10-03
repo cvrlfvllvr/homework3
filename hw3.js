ce8ab9d7aa5f0067cdc0135f936dbae1cd38cbb2
Carl Fuller <mr.carlfuller@gmail.com>
2019-10-03 12:21:29 -0700

    just started

---
+++ hw3.js
@@ -0,0 +1,10 @@
+if (confirm("Would you like a computer generated passcode?")) {
+    txt = "You said yes!";
+    console.log('you said yes');
+  } else {
+    txt = "You said no!";
+    console.log('you said no');
+    
+  }
+
+  if ()
